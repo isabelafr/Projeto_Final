a531512c5a7be768bdd0cb7a252df205d3fad3f9
Isabela Ribeiro <bellrribeiro@gmail.com>
2015-05-02 20:34:07 -0300

    Projeto Final
    Proposta do projeto

---
+++ Projeto_Final.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 20:31:51 2015
+
+@author: Isabela
+"""
+
+"""
+1- 	Nome do projeto: Python Man.
+2-	Integrantes: Bruna Di Bisceglie, Daniela Bento, Fernanda Dib e Isabela Ribeiro.
+3-	Potencial usuário do projeto: crianças, adolescentes e adultos que procuram um passatempo.
+4-	Descrição do produto: o jogo consistirá em labirintos na qual o jogador deverá coletar alguns itens para passar de fase. Ao mesmo tempo, o jogador será ameaçado por personagens que terão a função de tirar suas vidas, causando sua morte. 
+5-	Existem produtos similares como PacMan, o jogo em que baseamos o nosso e também Sneak que é parcialmente parecido.
+6-	Recursos utilizados: tutorias online, livros sobre jogos e sobre Python, YouTube, pesquisas no Google, funções, listas e loops do Python, e vários outros.
+7-	Planos B: 
+a) Fazer um jogo que consiste em somatórias, semelhante ao existente 
+2048
+;
+b) Fazer um jogo semelhante ao Uno, na qual os jogadores recebem 7 cartas e, seguindo as regras, quem acabar com suas cartas antes vence.
+"""
